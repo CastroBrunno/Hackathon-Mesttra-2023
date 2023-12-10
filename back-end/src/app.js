5badecb39b4c8bcbac73aa9b3c077db1ae7fe7ed
kellymoreira <kelly.moreira0510@gmail.com>
2023-12-10 15:23:57 -0300

    feat: implementando o swagger

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const routesPaciente = require('./routes/routesPaciente');
-const routesRegistroVacina = require('./routes/routesRegistroVacina');
+const express = require("express");
+const routesPaciente = require("./routes/routesPaciente");
+const routesRegistroVacina = require("./routes/routesRegistroVacina");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_output.json");
+
+app.use("/swagger-ui", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/paciente', routesPaciente);
-app.get('/', (req, res) => res.status(200).json('ok'))
-app.use('/registro-vacina', routesRegistroVacina)
+app.use("/paciente", routesPaciente);
+app.get("/", (req, res) => res.status(200).json("ok"));
+app.use("/registro-vacina", routesRegistroVacina);
 
 module.exports = app;
--- back-end/src/routes/routesPaciente.js
+++ back-end/src/routes/routesPaciente.js
@@ -1,15 +1,50 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const {createPacienteController, updatePacienteController, findAllPacienteController, findbyIdController} = require('../controller/pacienteController.js');
+const {
+  createPacienteController,
+  updatePacienteController,
+  findAllPacienteController,
+  findbyIdController,
+} = require("../controller/pacienteController.js");
 
-router.post('/', createPacienteController);
+router.post(
+  "/",
+  /*  #swagger.parameters['createPacienteController'] = {      
+    in: 'body',
+    type: 'object',
+    required: true,
+    '@schema': {
+      properties: {
+        id_paciente: {
+          type: "integer",          
+        },
+        nome: {
+          type: "string"
+        },
+        data_nascimento: {
+          type: "date",          
+        },
+      },
+      required: ["id_paciente", "nome", "data_nascimento",]
+    },
+  } */
+  createPacienteController
+);
 
-router.put('/:id', updatePacienteController);
+router.put("/:id", updatePacienteController);
 
-router.get('/', findAllPacienteController);
+router.get("/", findAllPacienteController);
 
-router.get('/:id', findbyIdController)
+router.get("/:id", findbyIdController);
+
+router.use(
+  "/albuns",
+  router
+  // #swagger.name = 'createPacienteRouter-controller'
+  // #swagger.description = 'createPaciente controller.'
+  // #swagger.tags = ['createPacienteController']
+);
 
 module.exports = router;
