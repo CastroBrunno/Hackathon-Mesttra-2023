2764be37015ba8436e27e9d94852c4371c0a0321
Bruno Sousa <brunocastroleandro@gmail.com>
2023-12-09 21:07:57 -0300

    feat: Implementação de consultas geral de vacinas

--- back-end/src/model/VacinaModel.js
+++ back-end/src/model/VacinaModel.js
@@ -1,11 +1,11 @@
 const pool = require('../connection/pool');
 
-const findVacinas = async (idVacina) =>{
-    const query = 'SELECT * FROM vacina'
+const findVacinas = async () =>{
+    const query = 'SELECT  vacina.*,  periodo_ano.*,  periodo_mes.*,  rede.* FROM  vacina full JOIN  periodoaplicacaoano AS periodo_ano ON vacina.Id_vacina = periodo_ano.Id_vacina full  join  periodoaplicacaomes AS periodo_mes ON vacina.Id_vacina = periodo_mes.Id_vacina full JOIN  rede AS rede ON vacina.Id_rede = rede.Id_rede;'
 
     const vacinas = await pool.query(query);
 
-    return vacinas;
+    return vacinas.rows;
 }
 
 module.exports = {
